70c81a86c685e80123c1db13a2b5391a2a5421be
Greyson Murray <gr.murray@draftkings.com>
2025-02-22 13:48:03 -0500

    Remove file-reading responsibility from lexer

--- src/Lexer.zig
+++ src/Lexer.zig
@@ -2,47 +2,39 @@ const std = @import("std");
 const Token = @import("Token.zig");
 
 const Allocator = std.mem.Allocator;
-const File = std.fs.File;
-
-const FileIoError = File.OpenError || File.ReadError;
-const Error = Allocator.Error || FileIoError;
-
 const Lexer = @This();
 
 pos: u32,
-file_contents: []u8,
+buf: []const u8,
 
-pub fn init(allocator: Allocator, file_path: []const u8) Error!*Lexer {
+pub fn init(allocator: Allocator, source_buf: []const u8) Allocator.Error!*Lexer {
     const lexer = try allocator.create(Lexer);
 
     lexer.* = .{
         .pos = 0,
-        .file_contents = try allocator.alloc(u8, 400),
+        .buf = source_buf,
     };
-
-    try readFile(file_path, lexer.file_contents);
 
     return lexer;
 }
 
 pub fn deinit(self: *Lexer, allocator: Allocator) void {
-    allocator.free(self.file_contents);
     allocator.destroy(self);
 }
 
 pub fn nextToken(self: *Lexer) Token {
-    const char = self.file_contents[self.pos];
+    const char = self.buf[self.pos];
 
     var kind: Token.Kind = undefined;
-    var value: []u8 = undefined;
+    var value: []const u8 = undefined;
     kind, value = switch (char) {
-        '#' =>  .{ .HEADING, self.lexHeading() },
-        '*' =>  .{ .ASTERISK, self.lexRepeating('*') },
+        '#' => .{ .HEADING, self.lexHeading() },
+        '*' => .{ .ASTERISK, self.lexRepeating('*') },
         '\n' => .{ .NEWLINE, self.lexRepeating('\n') },
-        '`' =>  .{ .CODE_FENCE, self.lexRepeating('`') },
-        '[', ']' =>  .{ .BRACKET_SQUARE, self.lexOne() },
-        '(', ')' =>  .{ .BRACKET_PAREN, self.lexOne() },
-        170 =>  .{ .EOF, "" },
+        '`' => .{ .CODE_FENCE, self.lexRepeating('`') },
+        '[', ']' => .{ .BRACKET_SQUARE, self.lexOne() },
+        '(', ')' => .{ .BRACKET_PAREN, self.lexOne() },
+        170 => .{ .EOF, "" },
         else => .{ .INLINE_TEXT, self.lexInlineText() }
     };
 
@@ -56,9 +48,9 @@ pub fn nextToken(self: *Lexer) Token {
     return token;
 }
 
-fn lexInlineText(self: *Lexer) []u8 {
+fn lexInlineText(self: *Lexer) []const u8 {
     const start_pos = self.pos;
-    var char: ?u8 = self.file_contents[self.pos];
+    var char: ?u8 = self.buf[self.pos];
 
     while (char != '\n') {
         if (char) |cur_char| {
@@ -74,9 +66,9 @@ fn lexInlineText(self: *Lexer) []u8 {
     return self.span(start_pos, end_pos);
 }
 
-fn lexHeading(self: *Lexer) []u8 {
+fn lexHeading(self: *Lexer) []const u8 {
     const start_pos = self.pos;
-    var char: ?u8 = self.file_contents[self.pos];
+    var char: ?u8 = self.buf[self.pos];
     var level: u32 = 0;
 
     while (char == '#' and level < 6) {
@@ -88,15 +80,15 @@ fn lexHeading(self: *Lexer) []u8 {
     return self.span(start_pos, end_pos);
 }
 
-fn lexOne(self: *Lexer) []u8 {
+fn lexOne(self: *Lexer) []const u8 {
     const start_pos = self.pos;
     _ = self.nextChar();
     const end_pos = self.pos;
     return self.span(start_pos, end_pos);
 }
 
-fn lexRepeating(self: *Lexer, repeating_char: u8) []u8 {
-    var char: ?u8 = self.file_contents[self.pos];
+fn lexRepeating(self: *Lexer, repeating_char: u8) []const u8 {
+    var char: ?u8 = self.buf[self.pos];
     const start_pos = self.pos;
 
     while (char == repeating_char) {
@@ -114,24 +106,15 @@ fn isTokenizableChar(char: u8) bool {
     };
 }
 
-fn span(self: *Lexer, start_pos: u32, end_pos: u32) []u8 {
-    return self.file_contents[start_pos..end_pos];
+fn span(self: *Lexer, start_pos: u32, end_pos: u32) []const u8 {
+    return self.buf[start_pos..end_pos];
 }
 
 fn nextChar(self: *Lexer) ?u8 {
-    if (self.pos > self.file_contents.len - 1) {
+    if (self.pos > self.buf.len - 1) {
         return null;
     }
 
     self.pos += 1;
-    return self.file_contents[self.pos];
-}
-
-fn readFile(file_path: []const u8, buffer: []u8) FileIoError!void {
-    const open_flags = File.OpenFlags { .mode = .read_only };
-
-    const file = try std.fs.openFileAbsolute(file_path, open_flags);
-    defer file.close();
-
-    _ = try file.readAll(buffer);
+    return self.buf[self.pos];
 }
--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,35 @@
 const std = @import("std");
 const Lexer = @import("lexer.zig");
+const File = std.fs.File;
 const Token = @import("Token.zig");
 
-const file_path = "/Users/gr.murray/Developer/zig/markdown-parser/samples/test.md";
+const sample_file_path = "/Users/gr.murray/Developer/zig/markdown-parser/samples/test.md";
+const FileIoError = File.OpenError || File.ReadError;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer { _ = gpa.deinit(); }
     const allocator = gpa.allocator();
 
-    const lexer = try Lexer.init(allocator, file_path);
-    defer lexer.deinit(allocator);
+    const source_buf = try allocator.alloc(u8, 400);
+    defer allocator.free(source_buf);
+    try readFile(sample_file_path, source_buf);
 
-    for (lexer.file_contents) |char| {
-        std.debug.print("{d}\n", .{ char });
-    }
+    const lexer = try Lexer.init(allocator, source_buf);
+    defer lexer.deinit(allocator);
 
     var token: Token = lexer.nextToken();
     while (token.kind != .EOF) {
         token = lexer.nextToken();
     }
 }
+
+fn readFile(file_path: []const u8, buffer: []u8) FileIoError!void {
+    const open_flags = File.OpenFlags { .mode = .read_only };
+
+    const file = try std.fs.openFileAbsolute(file_path, open_flags);
+    defer file.close();
+
+    _ = try file.readAll(buffer);
+}
+
