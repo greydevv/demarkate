ba4d278198e20045c215d472692fc0f0f3bf5286
Greyson Murray <gr.murray@draftkings.com>
2025-03-11 09:43:25 -0400

    Initial parser/ast implementation

---
+++ src/Parser.zig
@@ -0,0 +1,105 @@
+const std = @import("std");
+const ast = @import("ast.zig");
+const Tokenizer = @import("Tokenizer.zig");
+const Token = Tokenizer.Token;
+const Element = ast.Element;
+const Allocator = std.mem.Allocator;
+
+const Parser = @This();
+
+allocator: Allocator,
+tokenizer: *Tokenizer,
+elements: std.ArrayList(Element),
+errors: std.ArrayList(Error),
+
+pub fn init(allocator: Allocator, tokenizer: *Tokenizer) Parser {
+    return .{
+        .allocator = allocator,
+        .tokenizer = tokenizer,
+        // TODO: use assume capacity strategy that zig uses, ((tokens.len + 2) / 2)
+        .elements = std.ArrayList(Element).init(allocator),
+        .errors = std.ArrayList(Error).init(allocator),
+    };
+}
+
+pub fn deinit(self: *Parser) void {
+    for (self.elements.items) |*el| {
+        el.deinit();
+    }
+
+    self.elements.deinit();
+    self.errors.deinit();
+}
+
+pub fn parse(self: *Parser) !void {
+    loop: while (true) {
+        const token = self.tokenizer.next();
+        if (token.tag == .eof) {
+            break;
+        }
+
+        const el = switch (token.tag) {
+            .heading => blk: {
+                if (token.len() > 6) {
+                    return self.err(.heading_too_small, token);
+                }
+
+                const inline_el = try self.parseInline();
+                var node = Element.initNode(self.allocator, .heading);
+                try node.addChild(inline_el);
+                break :blk node;
+            },
+            .eof => break :loop,
+            else => return self.err(.unexpected_token, token),
+        };
+
+        try self.elements.append(el);
+    }
+}
+
+fn parseInline(self: *Parser) !Element {
+    const token = self.tokenizer.next();
+    if (token.tag != .literal_text) {
+        return self.err(.unexpected_token, token);
+    }
+
+    return Element.initLeaf(.text, token);
+}
+
+
+fn err(self: *Parser, tag: Error.Tag, token: Token) error{ ParseError, OutOfMemory } {
+    switch(tag) {
+        .heading_too_small =>
+            std.log.err(
+                "Heading too small ({})", .{
+                    token.len()
+                }
+            ),
+        // this is a catch-all for now
+        .unexpected_token =>
+            std.log.err(
+                "Unexpected token ({s}) from {} to {}", .{
+                    @tagName(token.tag),
+                    token.loc.start_index,
+                    token.loc.end_index 
+                }
+            ),
+    }
+
+    try self.errors.append(.{
+        .tag = tag,
+        .token = token,
+    });
+
+    return error.ParseError;
+}
+
+pub const Error = struct {
+    tag: Tag,
+    token: Token,
+
+    pub const Tag = enum {
+        heading_too_small,
+        unexpected_token,
+    };
+};
---
+++ src/ast.zig
@@ -0,0 +1,73 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const Token = @import("Tokenizer.zig").Token;
+
+pub const Element = union(Element.Type) {
+    node: Node,
+    leaf: Leaf,
+
+    pub const Type = enum {
+        node,
+        leaf
+    };
+
+    pub const Node = struct {
+        tag: Tag,
+        children: std.ArrayList(Element),
+
+        pub const Tag = enum {
+            heading
+        };
+
+        pub fn deinit(self: *Node) void {
+            for (self.children.items) |*child| {
+                child.deinit();
+            }
+
+            self.children.deinit();
+        }
+    };
+
+    pub const Leaf = struct {
+        tag: Tag,
+        token: Token,
+
+        pub const Tag = enum {
+            text,
+        };
+    };
+
+    pub fn initNode(allocator: Allocator, tag: Node.Tag) Element {
+        return .{
+            .node = Node{
+                .tag = tag,
+                // TODO: maybe init capacity?
+                .children = std.ArrayList(Element).init(allocator)
+            }
+        };
+    } 
+
+    pub fn initLeaf(tag: Leaf.Tag, token: Token) Element {
+        return .{
+            .leaf = Leaf{
+                .tag = tag,
+                .token = token
+            }
+        };
+    }
+
+    pub fn deinit(self: *Element) void {
+        switch (self.*) {
+            .node => |*n| n.deinit(),
+            .leaf => return,
+        }
+    }
+
+    pub fn addChild(self: *Element, child: Element) !void {
+        switch (self.*) {
+            .node => |*n| try n.children.append(child),
+            .leaf => return error.NotANode,
+        }
+    }
+};
+
